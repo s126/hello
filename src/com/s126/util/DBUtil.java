06bdd2e0b1a50611af1589ec205d92c9e4cbb334
a308143669 <136853241@qq.com>
2016-10-13 23:44:59 +0800

    添加db配置文件

--- src/com/s126/util/DBUtil.java
+++ src/com/s126/util/DBUtil.java
@@ -1,10 +1,13 @@
 package com.s126.util;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 
 /**
@@ -13,24 +16,39 @@ import java.sql.Statement;
  *
  */
 public class DBUtil {
-
-	private static String driver = "oracle.jdbc.driver.OracleDriver";
-	private static String url = "jdbc:oracle:thin:@127.0.0.1:1521:orcl";
-	private static String user = "vip";
-	private static String passwd = "vip";
-
+	
+	
+	private static String driver;
+	private static String url;
+	private static String user;
+	private static String password;
+	
 	static {
 		try {
+			
+			Properties prop = new Properties();
+			InputStream in = DBUtil.class.getClassLoader().getResourceAsStream("db.properties");
+			prop.load(in);
+			driver = prop.getProperty("driver");
+			url = prop.getProperty("url");
+			user = prop.getProperty("user");
+			password = prop.getProperty("password");
+			
 			Class.forName(driver);
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-		}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+		
+		
+		
 	}
 
 	public static Connection getCon() {
 		Connection conn = null;
 		try {
-			conn = DriverManager.getConnection(url, user, passwd);
+			conn = DriverManager.getConnection(url, user, password);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
